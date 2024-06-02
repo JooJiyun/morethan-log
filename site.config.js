ca28965bf8306e136bc174744a6e65ad7de992e9
jiyun <jiyun9762@gmail.com>
2024-06-02 13:48:40 +0900

    profile image 변경

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "jiyun",
-    image: "/blob/main/notion-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "application developer",
     bio: "",
     email: "jiyun9762@gmail.com",
